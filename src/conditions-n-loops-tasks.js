39ef27a347ae319a030e360f10412716f5b86bfb
Dinara Navshirvanova <dinaranav1987@gmail.com>
2025-11-11 19:57:42 +0400

    feat: solve shuffleChar task (not optimized)

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -411,8 +411,21 @@ function sortByAsc(/* arr */) {
  *  '012345', 3 => '024135' => '043215' => '031425'
  *  'qwerty', 3 => 'qetwry' => 'qtrewy' => 'qrwtey'
  */
-function shuffleChar(/* str, iterations */) {
-  throw new Error('Not implemented');
+function shuffleChar(str, iterations) {
+  let res = str;
+  for (let i = 0; i < iterations; i += 1) {
+    let odds = '';
+    let evens = '';
+    for (let j = 0; j < res.length; j += 1) {
+      if (j % 2 === 0) {
+        evens += res[j];
+      } else {
+        odds += res[j];
+      }
+    }
+    res = `${evens}${odds}`;
+  }
+  return res;
 }
 
 /**
